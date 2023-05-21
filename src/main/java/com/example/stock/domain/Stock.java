84bd3fff1634436ae2f7d418422f039999d383f7
ohy1023 <zvyg1023@naver.com>
2023-05-21 15:41:37 +0900

    재고 감소 로직 & 문제점

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,40 @@
+package com.example.stock.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if (this.quantity - quantity < 0) {
+            throw new RuntimeException("foo");
+        }
+        this.quantity -= quantity;
+    }
+
+}
