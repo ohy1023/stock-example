d703a67c60fe0d4fadd21499ed68dae89eca9e46
ohy1023 <zvyg1023@naver.com>
2023-05-21 16:44:39 +0900

    Named Lock

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -4,6 +4,7 @@ import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
@@ -12,7 +13,7 @@ public class StockService {
 
     private final StockRepository stockRepository;
 
-//    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity) {
 
         Stock stock = stockRepository.findById(id).orElseThrow();
